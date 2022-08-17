074e85b1616ec7f42638e33491e15183620091af
Bones404 <75161173+404Bones@users.noreply.github.com>
2022-08-17 20:22:22 +0200

    Added source code

---
+++ ModDescription.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BepInEx;
+using System.IO;
+
+namespace Album
+{
+    public class ModDescription
+    {
+        public ModDescription(string modname, string desc)
+        {
+            string albumdescpath = Paths.BepInExRootPath + "/albumdesc/";
+            Directory.CreateDirectory(albumdescpath);
+            if (File.Exists(albumdescpath + modname.ToLower() + ".txt")) {
+                return;
+            } else {
+                var descfile = File.CreateText(albumdescpath + modname.ToLower() + ".txt");
+                descfile.Write(desc);
+                descfile.Close();
+            }  
+        }
+    }
+}
---
+++ ResourceLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Reflection;
+using UnityEngine;
+using System.Linq;
+
+namespace Album
+{
+    public static class EmbeddedResourceLoader
+    {
+        public static Object Load(string name)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = assembly.GetManifestResourceNames().First(r => r.Contains(name));
+            var resource = assembly.GetManifestResourceStream(resourceName);
+            using var memoryStream = new MemoryStream();
+            var buffer = new byte[16384];
+            int count;
+            while ((count = resource!.Read(buffer, 0, buffer.Length)) > 0)
+                memoryStream.Write(buffer, 0, count);
+            var spriteTexture = new Texture2D(0, 0, TextureFormat.ARGB32, false)
+            {
+                anisoLevel = 1,
+                filterMode = 0
+            };
+
+            ImageConversion.LoadImage(spriteTexture, memoryStream.ToArray());
+            return spriteTexture;
+        }
+    }
+}
